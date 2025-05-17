2cfb3779bc9bc3ecef157e4f81bdb1fa60e7c63e
shirokovvia4eslav <shirokovvia4eslav@yandex.ru>
2025-05-17 20:19:30 +0400

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,8 @@
+# URL_SERVICE хранит базовый URL тестового стенда, который используется для доступа к API
+URL_SERVICE = "https://c39b60b0-34e2-4d0d-a819-5a70b612b841.serverhub.praktikum-services.ru/"
+
+# CREATE_ORDER_PATH хранит путь к API-методу для создания нового заказа.
+CREATE_ORDER_PATH = "/api/v1/orders"
+
+# CHECK_ORDER_PATH хранит путь к API-методу для проверки заказа по его номеру.
+CHECK_ORDER_PATH = "/api/v1/orders/track?t="
